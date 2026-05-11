ad43bc61e08e1b162e6b99910f0fe8a9ad826778
Chris <cr1s@hotmail.it>
2026-05-11 16:22:22 +0200

    add NotesColumn

--- app/src/main/java/com/example/jetnoteappwm/MainActivity.kt
+++ app/src/main/java/com/example/jetnoteappwm/MainActivity.kt
@@ -1,18 +1,32 @@
 package com.example.jetnoteappwm
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetnoteappwm.data.NotesDataSource
+import com.example.jetnoteappwm.model.Notes
 import com.example.jetnoteappwm.screen.NoteScreen
 import com.example.jetnoteappwm.ui.theme.JetNoteAppWMTheme
+import java.time.format.DateTimeFormatter
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +38,7 @@ class MainActivity : ComponentActivity() {
                 ){ innerPadding ->
                     NoteScreen(
                         modifier = Modifier.padding(innerPadding),
-                        notes = emptyList(),
+                        notes = NotesDataSource().loadNotes(),
                         onAddNote = {},
                         onRemoveNote = {})
 
--- app/src/main/java/com/example/jetnoteappwm/screen/NoteScreen.kt
+++ app/src/main/java/com/example/jetnoteappwm/screen/NoteScreen.kt
@@ -1,14 +1,27 @@
 package com.example.jetnoteappwm.screen
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Create
+import androidx.compose.material.icons.rounded.DateRange
+import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -19,6 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,7 +40,9 @@ import androidx.compose.ui.unit.dp
 import com.example.jetnoteappwm.R
 import com.example.jetnoteappwm.components.NoteButton
 import com.example.jetnoteappwm.components.NoteInputText
+import com.example.jetnoteappwm.data.NotesDataSource
 import com.example.jetnoteappwm.model.Notes
+import java.time.format.DateTimeFormatter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -42,12 +58,14 @@ fun NoteScreen(
     Column(modifier = Modifier
         .padding(6.dp)) {
         TopAppBar(title = {
-            Text(stringResource(id = R.string.app_name))
+            // Text(stringResource(id = R.string.app_name))  //prendo il nome dal res. app string name
+            Text(text = "JetNoteByChris", color = Color.White)
         }, actions = {
-            Icon(Icons.Rounded.Notifications, contentDescription = "App Icon")
+            Icon(Icons.Rounded.Edit, contentDescription = "App Icon", tint = Color.White)
         },
             colors = TopAppBarDefaults.mediumTopAppBarColors(
-                containerColor = Color(0xFF9DB5CE)))
+                containerColor = Color(0xFF9FC2E3)
+            ))
 
         //TextField
         Column(modifier = Modifier.fillMaxWidth().padding(5.dp),
@@ -81,19 +99,59 @@ fun NoteScreen(
             NoteButton(
                 modifier = Modifier.padding(top = 8.dp, bottom = 8.dp),
                 text = "Save",
+
                 onClick = {
-                    if (title.isNotEmpty() && description.isNotEmpty())
+                    if (title.isNotEmpty() && description.isNotEmpty()) {
                         //save/add to list
                         //and after clear text
                         title = ""
                         description = ""
-                })
+                    }
+                }
+            )
+        }
+        Divider(modifier = Modifier.padding(10.dp))
+        //list of notes
+        LazyColumn {
+            items(notes){ note ->
+                NoteRow(note = note,
+                    onNoteClicked =
+                        { }
+                )
+            }
         }
     }
 }
 
+
+@Composable
+fun NoteRow(
+    modifier: Modifier = Modifier,
+    note: Notes,
+    onNoteClicked: (Notes) -> Unit
+){
+    Surface(modifier.padding(4.dp)
+        .clip(RoundedCornerShape(topEnd = 33.dp, bottomStart = 33.dp))
+        .fillMaxWidth(),
+        color = Color(0xFFB8D9E7),
+        shadowElevation = 7.dp
+    ){
+        Column(modifier = Modifier.clickable { }.padding(horizontal = 14.dp, vertical = 6.dp)
+        ){
+            Text(text = note.title, style = MaterialTheme.typography.titleMedium)
+            Text(text = note.description, style = MaterialTheme.typography.titleSmall)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                Text(text = note.entryDate.format(
+                    DateTimeFormatter.ofPattern("EEE d MMM")))
+            }
+        }
+    }
+}
+
+
+
 @Preview(showBackground = true)
 @Composable
 fun NoteScreenPreview(){
-    NoteScreen(notes = emptyList(), onAddNote = {}, onRemoveNote = {})
+    NoteScreen(notes = NotesDataSource().loadNotes(), onAddNote = {}, onRemoveNote = {})
 }
